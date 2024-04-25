212e0d6a10b7f380fb8077bba3045e04c7e148fd
Matheus <mattanitelli4@gmail.com>
2024-04-25 20:56:21 -0300

    Projeto finalizado

--- Escalonamento.py
+++ Escalonamento.py
@@ -80,10 +80,10 @@ def scan(acessos, taman_fila):
             #Se o escalonador estiver indo para a direita e o maior elemento já estiver escalonado, troca de sentido
             if(max(qntdFila)<posicao_atual and sentido == 1):
                 sentido = -1
-                cilindrosPercorridos += (TAMANHO -posicao_atual)
-                posicao_atual = CILINDRO
+                cilindrosPercorridos += (CILINDRO -posicao_atual-1)
+                posicao_atual = CILINDRO-1
                 
-            #Se escalonador escalonador estiver indo para esquerda e o menor elemento já estiver escalonado, troca de sentido
+            #Se escalonador estiver indo para esquerda e o menor elemento já estiver escalonado, troca de sentido
             elif(min(qntdFila)>posicao_atual and sentido == -1):
                 sentido = 1
                 cilindrosPercorridos +=posicao_atual
@@ -121,8 +121,10 @@ def cscan(acessos, taman_fila):
             #Verificando se o maior valor já foi escalonado
             if(max(qntdFila)<posicao_atual):
                 #Volta o escalonador para 0 e adiciona o movimento fisico
-                posicao_atual = min(qntdFila)
-                cilindroFisico += CILINDRO
+                cilindrosPercorridos += (CILINDRO - 1 - posicao_atual)
+                cilindroFisico += (CILINDRO - 1 - posicao_atual)
+                posicao_atual = 0
+                cilindroFisico += CILINDRO-1
             
     return cilindrosPercorridos, cilindroFisico
 
@@ -145,6 +147,3 @@ for tamanho in lista_tamanho:
     print(f"CSCAN: {resultadoCscan} -> Cilindro virtual / Cilindro Fisico")
 
     print("===============================================================")
-
-
-   
